614eff532c6851a1f42cf5ee5b5fb7a7d53ccf50
Dennis <nndenninn@gmail.com>
2020-05-09 11:33:22 +0300

    Added new functionality

--- Project4-iOS13/ViewController.swift
+++ Project4-iOS13/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
+    
+    override func loadView() {
+        super.loadView()
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        if let url = URL(string: "https://" + websites[0]) {
+                self.webView.load(URLRequest(url: url))
+                self.webView.allowsBackForwardNavigationGestures = true
+        }
+        
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton;
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 
-
+    @objc func openTapped() {
+        let alertController = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        for website in websites {
+            alertController.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(alertController, animated: true)
+    }
+    
+    @objc func back(sender: UIBarButtonItem) {
+        if(webView.canGoBack) {
+             webView.goBack()
+        } else {
+            self.navigationController?.popViewController(animated:true)
+        }
+    }
+    
+    func openPage(action: UIAlertAction) {
+        if let url = URL(string: "https://" + action.title!) {
+            self.webView.load(URLRequest(url: url))
+        }
+    }
 }
 
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+
+        decisionHandler(.cancel)
+        let alertController = UIAlertController(title: "Warning!", message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .cancel))
+        present(alertController, animated: true)
+    }
+}
